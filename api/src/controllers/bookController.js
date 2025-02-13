f8ffc8dce5d6a45949bbbe3cda47759e5de1b916
michelekluck <michelenamur27@gmail.com>
2025-02-13 00:07:13 -0300

    create get and delete book

--- api/src/controllers/bookController.js
+++ api/src/controllers/bookController.js
@@ -3,11 +3,11 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+// criar livro
 const createBook = async (req, reply) => {
     try {
         const { title, category, year, description, authors } = req.body;
 
-        //criando livro
         const book = await prisma.book.create({
             data: { 
                 title, 
@@ -73,4 +73,55 @@ const createBook = async (req, reply) => {
     }
 };
 
-export { createBook };
+// listar todos os livros
+const getBooks = async (req, reply) => {
+    try {
+        const books = await prisma.book.findMany({
+            include: {
+                book_author: {
+                    include: {
+                        author: true
+                    }
+                }
+            }
+        });
+
+        reply.send(books);
+    } catch (error) {
+        console.error(error);
+        reply.status(500).send({error: 'Erro ao buscar livros'});
+    }
+};
+
+// deletar livro
+const deleteBook = async (req, reply) => {
+    try {
+        const { id } = req.params; // id do livro a ser excluido
+
+        // verificar se o livro existe
+        const book = await prisma.book.findUnique({
+            where: { id: parseInt(id) },
+        });
+
+        if (!book) {
+            return reply.status(404).send({ error: 'Livro não encontrado' });
+        }
+
+        // deletar a relação de autores (se houver)
+        await prisma.book_author.deleteMany({
+            where: { book_id: parseInt(id) },
+        });
+
+        // deletar o livro
+        await prisma.book.delete({
+            where: { id: parseInt(id) },
+        });
+
+        reply.status(200).send({ message: 'Livro deletado com sucesso!' });
+    } catch (error) {
+        console.log(error);
+        reply.status(500).send({ error: 'Erro ao deletar livro'});
+    }
+}
+
+export { createBook, getBooks, deleteBook };
--- api/src/routes/bookRoutes.js
+++ api/src/routes/bookRoutes.js
@@ -1,7 +1,15 @@
-import { createBook } from '../controllers/bookController.js';
+import { createBook, getBooks, deleteBook } from '../controllers/bookController.js';
 
 async function bookRoutes(fastify, options) {
+    //POST
     fastify.post('/books', createBook);
+
+    //GET
+    fastify.get('/books', getBooks);
+
+    //DELETE
+    fastify.delete('/books/:id', deleteBook);
 }
+
 
 export default bookRoutes;
